d1b4e5678cd3c8210bbe9b2a4cd893570e924eba
Celebration-cloud <ojinguluc@gmail.com>
2024-07-26 21:39:05 +0100

    order fixs

--- .history/src/components/cart/CartComponent_20240726213810.jsx
+++ .history/src/components/cart/CartComponent_20240726213810.jsx
@@ -57,7 +57,7 @@ const CartComponent = () => {
         orderId: uuid(),
         cart: cartListRef.current,
         user: userData[0]?.user_name,
-        email: userData[0]?.email,
+        email: userDate
         status: "pending",
         total: overallSum + cartTotalsData.shipping,
         address: userData[0]?.address,
