5a3f073b5856cfe020a7ef1525da6f31c37ef8df
Celebration-cloud <ojinguluc@gmail.com>
2024-08-17 15:50:58 +0100

    rating

---
+++ .history/src/components/details/Content_20240817154126.jsx
@@ -0,0 +1,218 @@
+/* eslint-disable react/prop-types */
+import CarouselImage from "./CarouselImage";
+import { useCallback, useEffect, useRef, useState } from "react";
+import styles from "./Details.module.css";
+import Counter from "../reuseable/counter/Counter";
+import Button from "../reuseable/button/Button";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getItemDetails } from "../../store/actions/quoteActions";
+import { userCartList, userRatingData, userWishlist } from "../../store/actions/userActions";
+import { toaster } from "evergreen-ui";
+function Content({preview}) {
+  const { session } = useSelector((state) => state.user);
+  const { userData } = useSelector((state) => state.user);
+  const { productDetails } = useSelector((state) => state.product);
+  const cartListRef = useRef([]);
+  cartListRef.current = userData ? userData[0]?.cart : [];
+  const wishlistRef = useRef();
+  wishlistRef.current = userData ? userData[0]?.wishlist : [];
+  const [count, setCount] = useState(0)
+  const { name } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const product = productDetails[0]
+  const productRatings = useRef();
+  productRatings.current = product?.rating;
+
+  const ratings = product.rating?.map((item) => item.rating) || [];
+  const totalSum = ratings.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  const userRating =
+    product.rating?.find((item) => item.name === userData[0]?.user_name) ||
+    null;
+  console.log(product)
+  console.log(productRatings.current, userRating);
+
+  const handleRatingChange = (event, newValue) => {
+    if (!session) return navigate("/log");
+    if (userRating) {
+      const updateRate = productRatings.current.map((item) =>
+        item.name === userData[0]?.user_name
+          ? { ...item, rating: newValue }
+          : item
+      );
+      productRatings.current = updateRate;
+      dispatch(userRatingData(productRatings.current, product?.product));
+      console.log(productRatings.current, product?.product);
+    } else {
+      const newRate = { name: userData[0]?.user_name, rating: newValue };
+      productRatings.current = [...productRatings.current, newRate];
+      console.log(productRatings.current, product?.product);
+      dispatch(userRatingData(productRatings.current, product?.product));
+    }
+  };
+
+  console.log(`Total Sum of Ratings: ${totalSum % 5}`);
+function handleWish(item) {
+  if (session) {
+    if (userData) {
+      // Check if the item is already in the wishlist
+      const existingIndex = wishlistRef.current?.findIndex(
+        (existingItem) => existingItem.id === item.id
+      );
+
+      if (existingIndex !== -1) {
+        // Item is already in the wishlist, remove it
+        wishlistRef.current.splice(existingIndex, 1);
+        dispatch(userWishlist(wishlistRef.current, userData[0].user_name));
+        
+          dispatch(getItemDetails(name));
+        
+        toaster.success("Removed from wishlist");
+      } else {
+        // Item is not in the wishlist, add it
+        wishlistRef.current = [...wishlistRef.current, item];
+        dispatch(userWishlist(wishlistRef.current, userData[0].user_name));
+        
+          dispatch(getItemDetails(name));
+        
+        toaster.success("Added to wishlist");
+      }
+    }
+  } else {
+    navigate("/log"); // Redirect to login page if not authenticated
+  }
+}
+
+
+  const fet = useCallback(() => {
+      dispatch(getItemDetails(preview || name));
+     wishlistRef.current = userData ? userData[0]?.wishlist : [];
+  }, [dispatch, name, userData, preview]);
+  useEffect(() => {
+    
+      fet();
+    
+  }, [fet]);
+  const updateQuantity = (newQuantity) => {
+    // Find the item in the cart based on itemId
+    setCount(newQuantity)
+  };
+  function handleCart(item) {
+    if (session) {
+      if (userData) {
+        // Check if the item is already in the wishlist
+        const existingIndex = cartListRef.current?.findIndex(
+          (existingItem) => existingItem.id === item.id
+        );
+        
+        if (existingIndex !== -1) {
+          // Item is already in the wishlist, remove it
+          toaster.warning("Item is already in the cart");
+        } else {
+          // Item is not in the wishlist, add it
+        if(count === 0){
+          toaster.warning("Quantity must be greater than 0")
+        }else{
+          cartListRef.current = [
+            ...cartListRef.current,
+            { ...item, amount: count },
+          ];
+          dispatch(userCartList(cartListRef.current, userData[0].user_name));
+          toaster.success("Added to cart");
+        } 
+          
+        }
+      }
+    } else {
+      navigate("/log");
+    }
+  }
+  const exist = wishlistRef.current?.find(
+    (item) => item.id === productDetails[0]?.id
+  ); 
+  return (
+    <div className={styles.content}>
+      {productDetails?.map((item) => (
+        <>
+          <div key={item.id} className={styles.image}>
+            <CarouselImage image={item.img} />
+          </div>
+
+          <div className={styles.info}>
+            <strong className={styles.title}>{item.product}</strong>
+            <strong>Brand: {item.brand}</strong>
+            <h3>Quantity: {item.quantity}</h3>
+            <span className={styles.price}>
+              <strong>&#x20A6;{item.price.toFixed(2)}</strong>
+            </span>
+            <span className="flex items-center">
+              <span>Rating:</span>
+              <Ratingg value={totalSum % 5} />
+            </span>
+            <br />
+            <span className="flex items-center">
+              <span>Your Rating:</span>
+              <Rating
+                value={userRating?.rating}
+                onChange={handleRatingChange}
+              />
+            </span>
+            <section className={styles.selection}>
+              {item.quantity > 0 && (
+                <div>
+                  <Counter
+                    onQuantityChange={(count) => updateQuantity(count)}
+                  />
+                </div>
+              )}
+
+              <div>
+                <Button
+                  disabled={item.quantity < 1}
+                  onClick={() => handleCart(item)}
+                  name={item.quantity < 1 ? "Out of Stock" : "Add to Cart"}
+                />
+              </div>
+            </section>
+            <section className={styles.wishing}>
+              <span>Open sizes guide</span>
+              <span className={styles.wish}>
+                {exist ? (
+                  <i
+                    onClick={() => handleWish(item)}
+                    className="fa-solid fa-heart fa-xl"
+                  ></i>
+                ) : (
+                  <i
+                    onClick={() => handleWish(item)}
+                    className="fa-regular fa-heart fa-xl"
+                  ></i>
+                )}
+                <span>Add to Wishlist</span>
+              </span>
+            </section>
+            <section className={styles.describe}>
+              <h2>Description:</h2>
+              <p>{item.description}</p>
+            </section>
+            <section className={styles.share}>
+              <h5>Share</h5>
+              <span>
+                <i className="fa-brands fa-facebook fa-xl"></i>
+                <i className="fa-brands fa-instagram fa-xl"></i>
+                <i className="fa-brands fa-square-whatsapp fa-xl"></i>
+              </span>
+            </section>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+}
+
+export default Content;
---
+++ .history/src/components/reuseable/card/CardComponent_20240817142654.jsx
@@ -0,0 +1,164 @@
+/* eslint-disable react/prop-types */
+import { Rating } from "@mui/material";
+import ImageItem from "./ImageItem";
+import style from './CardComponent.module.css'
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toaster } from "evergreen-ui";
+import { userCartList } from "../../../store/actions/userActions";
+import { useState } from "react";
+const CardComponent = ({ product, wishlistRef, cartListRef }) => {
+const { session } = useSelector((state) => state.user);
+const { userData } = useSelector((state) => state.user);
+const navigate = useNavigate();
+const dispatch = useDispatch();
+const [productRatings, setProductRatings] = useState([]);
+const [rating, setRating] = useState(0);
+
+// Handle rating change
+const ratings = product?.rating.map((item) => item.rating) || [];
+// const [usersRatings] = useState([
+//   { name: userData[0]?.user_name, rating: 4 },
+//   { name: "Bob", rating: 3 },
+//   { name: "Charlie", rating: 4 },
+//   { name: "Diana", rating: 4 },
+//   { name: "Ethan", rating: 3 },
+// ]);
+const userRating = productRatings.find(
+  (item) => item.name === userData[0]?.user_name
+);
+
+// Calculate the total sum of ratings
+const totalSum = ratings.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+console.log(productRatings, userRating);
+
+const handleRatingChange = (event, newValue) => {
+  if (!session) return navigate("/log");
+  setRating(newValue);
+
+  if (userRating) {
+    const updateRate = product?.rating.map((item) =>
+  item.name === userData[0]?.user_name
+    ? { ...item, rating: newValue }
+    : item
+);
+
+    setProductRatings((prev) => prev {return {.}});
+  } else {
+    productRatings.push({name: userData[0]?.user_name, rating: newValue})
+    console.log(productRatings);
+  }
+};
+
+console.log(`Total Sum of Ratings: ${totalSum % 5}`);
+
+
+  const styles = {
+    productPrice: {
+      fontSize: "16px",
+      fontWeight: "bold",
+    },
+    originalPrice: {
+      textDecoration: "line-through",
+      color: "#777",
+      fontSize: "14px",
+    },
+  };
+  function handleDetails() {
+    navigate(`/Shop/${product?.product}`);
+  }
+  function handleCart (item) {
+    if (session) {
+      if (userData) {
+        // Check if the item is already in the wishlist
+        const existingIndex = cartListRef.current?.findIndex(
+          (existingItem) => existingItem.id === item.id
+        );
+
+        if (existingIndex !== -1) {
+          // Item is already in the wishlist, remove it
+          toaster.warning("Item is already in the cart");
+        } else {
+            const existingIndex = cartListRef.current?.findIndex(
+              (existingItem) => existingItem.quantity > 1
+            );
+            if (existingIndex !== -1) {
+              return toaster.warning("Out of stock")
+
+            } else {
+              // Item is not in the wishlist, add it
+              cartListRef.current = [
+                ...cartListRef.current,
+                { ...item, amount: 1 },
+              ];
+              dispatch(
+                userCartList(cartListRef.current, userData[0].user_name)
+              );
+
+              toaster.success("Added to cart");
+            }
+          
+        }
+      }
+    } else {
+      navigate("/log");
+    }
+  }
+
+  return (
+    <div className={style.card}>
+      <div className={style["card-img"]}>
+        <ImageItem product={product} wishlistRef={wishlistRef} />
+      </div>
+      <div className={style["card-info"]}>
+        <p onClick={handleDetails} className={style["text-title"]}>
+          {product?.product}
+        </p>
+        <p onClick={handleDetails} className={style["text-body"]}>
+          {product.description?.slice(0, 40)}
+          <br />
+          Category: <span style={{ color: "blue" }}>{product.category}</span>
+        </p>
+        <h3>Quantity: {product.quantity}</h3>
+        <span className="flex items-center">
+          <span>Rating:</span>
+          <Rating value={totalSum % 5} />
+        </span>
+        <br />
+        <span className="flex items-center">
+          <span>Your Rating:</span>
+          <Rating value={rating} onChange={handleRatingChange} />
+        </span>
+      </div>
+      <div className={style["card-footer"]}>
+        <span className={style["text-title"]}>
+          <strong style={{ display: "inline-flex", gap: "10px" }}>
+            <span style={styles.productPrice}>&#x20A6;{product?.price}</span>
+            {product?.originalPrice && (
+              <span style={styles.originalPrice}>
+                &#x20A6;{product?.originalPrice}
+              </span>
+            )}
+          </strong>
+        </span>
+        {product?.quantity > 0 && (
+          <div
+            onClick={() => handleCart(product)}
+            className={style["card-button"]}
+          >
+            <svg className={style["svg-icon"]} viewBox="0 0 20 20">
+              <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
+              <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
+              <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
+            </svg>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
